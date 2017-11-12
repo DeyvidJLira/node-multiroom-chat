82b3b00c5cd6ba796a5bf46dce60bd9b68f61f3d
Deyvid Lira <deyvidjlira@gmail.com>
2017-11-12 11:55:27 -0200

    Creates connection by websocket

--- app.js
+++ app.js
@@ -6,4 +6,13 @@ var server = app.listen(80, function() {
     console.log("Server ON");
 });
 
-require('socket.io').listen(server);
+var io = require('socket.io').listen(server);
+
+//Create a connection by websocket
+io.on('connection', function(socket) {
+    console.log("User connected");
+
+    socket.on('disconnect', function() {
+        console.log("User disconnected");
+    });
+});
